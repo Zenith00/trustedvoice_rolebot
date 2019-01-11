973fa0bebe7abf3ea08a9d1c211f87a144a477da
Austin Zhang <zenith00dev@gmail.com>
2019-01-10 16:28:54 -0800

    V1

--- CONFIG.py
+++ CONFIG.py
@@ -1,4 +1,19 @@
-
 BOT_TOKEN = ""
 PREFIX = "!"
-LOGGING_LEVEL = "WARNING"
+LOGGING_LEVEL = "INFO"
+ACK_TYPE = "react"
+ROLE_TO_ID = {"SR_Shrubbery_Hiraeth"       : 533038573792919553,
+              "SR_Shrubbery_Grey"          : 533028882035769374,
+              "SRSuzuya𝖚𝖓𝖙𝖎𝖙𝖑𝖊𝖉"             : 532990712325865492,
+              "SR_ChouTofu_DERANGEDcitiZen": 532980885297823785,
+              "Member"                     : 426861484983975937,
+              "Muted"                      : 426487777669152770,
+              "Offering"                   : 427624125679403008,
+              "Looking"                    : 427624132134567937,
+              "TrustedVoices"              : 426487730382569483,
+              "Chat-Moderator-"            : 446642041191923712,
+              "Chat-Moderator"             : 426487602691047460,
+              "KindVoice-Moderator"        : 426487509183234060,
+              "Bots"                       : 441181488789323777}
+
+ciID_TO_ROLE = ciROLE_TO_ID = ID_TO_ROLE = {}
--- rolebot.py
+++ rolebot.py
@@ -6,6 +6,10 @@ import discord
 
 logging.basicConfig(level=CONFIG.LOGGING_LEVEL)
 
+CONFIG.ID_TO_ROLE = dict([(v, k) for k, v in CONFIG.ROLE_TO_ID.items()])
+CONFIG.ciID_TO_ROLE = dict([(v, k.lower()) for k, v in CONFIG.ROLE_TO_ID.items()])
+CONFIG.ciROLE_TO_ID = dict([(k.lower(), v) for k, v in CONFIG.ROLE_TO_ID.items()])
+
 class MicroClient(discord.Client):
     commands = {}
 
@@ -16,31 +20,98 @@ class MicroClient(discord.Client):
         logging.info("Connected")
 
     async def on_message(self, message):
-        content = message.content # type:str
         if message.content.startswith(CONFIG.PREFIX):
+            command_raw = message.content[len(CONFIG.PREFIX):].lower()
+            if command_raw in self.commands:
+                await self.commands[command_raw].execute(Contexter(message))
 
     @zutils.parametrized
-    def command(self, name: str = None, **attrs):
-        def deco(func):
-            return Command(func, fname=name, **attrs)
-        return deco
+    def command(func, self, name: str = None, **attrs):
+        logging.info(f"Registered function: func: {func}, override name = {name}")
+        command = Command(func, fname=name, **attrs)
+        self.add_command(command)
+        return command
 
-    def add_command(self):
-        pass
-
+    def add_command(self, command):
+        self.commands[command.fname] = command
 
 class Command:
-    def __init__(self, func: function, fname: str = None, **kwargs):
+    def __init__(self, func, pre=None, post=None, fname: str = None, **kwargs):
+        self.fname = fname
         self.func = func
+        self.pres = []
+        self.posts = []
         self.case_sens = kwargs.get("case_sens", True)
-        if not fname:
-            self.fname = self.func.__name__  # type:str
+        self.ack = kwargs.get("ack", "")
+        if not self.fname:
+            self.fname = func.__name__  # type:str
+        self.fname = self.fname.lower()
+
+        if self.ack == "react":
+            async def add_checkmark(ctx):
+                await ctx.m.add_reaction("✅")
+
+            self.posts.append(add_checkmark)
+
+        elif self.ack == "delete":
+            async def delete_m(ctx):
+                await ctx.m.delete()
+
+            self.posts.append(delete_m)
+
+    async def execute(self, ctx):
+        [await pre(ctx) for pre in self.pres]
+        await self.func(ctx)
+        [await post(ctx) for post in self.posts]
+
+class Contexter:
+    def __init__(self, message):
+        self.m = message
+
+    def find_role(self, query):
+        if isinstance(query, int):
+            return next(role for role in self.m.guild.roles if role.id == query)
+        if isinstance(query, str):
+            try:
+                res = next(role for role in self.m.guild.roles if role.name == query)
+            except StopIteration:
+                res = next(role for role in self.m.guild.roles if role.name.lower() == query.lower())
+            return res
+
+client = MicroClient()
+
+# Offering
+@client.command(ack=CONFIG.ACK_TYPE)
+async def offering(ctx: Contexter):
+    await ctx.m.author.add_roles(ctx.find_role("offering"))
+
+@client.command(ack=CONFIG.ACK_TYPE)
+async def offeringt(ctx: Contexter):
+    if ctx.find_role("offering") in ctx.m.author.roles:
+        await ctx.m.author.remove_roles(ctx.find_role("offering"))
+    else:
+        await ctx.m.author.add_roles(ctx.find_role("offering"))
+
+@client.command(name="-offering", ack=CONFIG.ACK_TYPE)
+async def deoffering(ctx):
+    await ctx.m.author.remove_roles(ctx.find_role("offering"))
+
+# Seeking
+@client.command(ack=CONFIG.ACK_TYPE)
+async def seeking(ctx: Contexter):
+    await ctx.m.author.add_roles(ctx.find_role("seeking"))
+
+@client.command(ack=CONFIG.ACK_TYPE)
+async def seekingt(ctx: Contexter):
+    if ctx.find_role("seeking") in ctx.m.author.roles:
+        await ctx.m.author.remove_roles(ctx.find_role("seeking"))
+    else:
+        await ctx.m.author.add_roles(ctx.find_role("seeking"))
+
+@client.command(name="-seeking", ack=CONFIG.ACK_TYPE)
+async def deseeking(ctx):
+    await ctx.m.author.remove_roles(ctx.find_role("seeking"))
 
 
-@MicroClient.command(case_sens=False)
-def offering(ctx):
-    pass
 
-
-MicroClient.run(CONFIG.BOT_TOKEN, bot=False)
-
+client.run(CONFIG.BOT_TOKEN, bot=True)
